c0dd27f961994802574e9d5e54d83312bbba83b5
Emily <emilys@genstudents.org>
2025-01-28 13:49:38 -0300

    :sparkles: add: Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,16 @@ import { ProdutoModule } from './produtos/produto.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_farmacia_go',
-      autoLoadEntities: true, 
-      synchronize: true, 
-    }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+	    useClass: ProdService,
+      imports: [ConfigModule],
+}),
     CategoriaModule,
     ProdutoModule,
     AuthModule,
